c7be88758bca8d207d94e83e56ca3951cd2f61db
LeLe86 <53364132+LeLe86@users.noreply.github.com>
2024-03-14 16:23:33 +0800

    把 setupPackage.py中的源改为清华的

--- setupPackage.py
+++ setupPackage.py
@@ -5,4 +5,4 @@ from subprocess import call
 #如果从默认源安装比较慢的话直接运行这个文件安装
 lst=["beautifulsoup4","lxml","requests"]
 for pkg in lst:
-    call("pip install -i https://pypi.douban.com/simple --upgrade " + pkg)
+    call("pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade " + pkg)
